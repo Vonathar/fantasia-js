0ad58b8daa9cd3a9a6d1760da42758722d9dafb5
Vonathar <gianmarco.caputo.uk@gmail.com>
2019-05-23 13:47:56 +0100

    Updates

---
+++ precache-manifest.a5f460c2eec456da5d6b1eff8b03f554.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2245f5cd989785b24254363a688fb0",
+    "url": "/fantasia-js/index.html"
+  },
+  {
+    "revision": "0544c558e57013071a27",
+    "url": "/fantasia-js/static/css/2.a4c6d6ca.chunk.css"
+  },
+  {
+    "revision": "5ff3144d7a68a490c9b6",
+    "url": "/fantasia-js/static/css/main.f95a9e21.chunk.css"
+  },
+  {
+    "revision": "0544c558e57013071a27",
+    "url": "/fantasia-js/static/js/2.de7861dc.chunk.js"
+  },
+  {
+    "revision": "5ff3144d7a68a490c9b6",
+    "url": "/fantasia-js/static/js/main.4538a7cf.chunk.js"
+  },
+  {
+    "revision": "369c4ecfd27df2315bc5",
+    "url": "/fantasia-js/static/js/runtime~main.f33bc8dc.js"
+  },
+  {
+    "revision": "81fbd0566147abf56c4e0fc549f94985",
+    "url": "/fantasia-js/static/media/enemy_1.81fbd056.png"
+  },
+  {
+    "revision": "4ac53ed4897ce3dfbcadfb1411c95844",
+    "url": "/fantasia-js/static/media/enemy_10.4ac53ed4.png"
+  },
+  {
+    "revision": "088396cd340197931feb515f0bf26ccd",
+    "url": "/fantasia-js/static/media/enemy_11.088396cd.png"
+  },
+  {
+    "revision": "46c91ff4f0f3a9ddcfd59ef5d8adcfd8",
+    "url": "/fantasia-js/static/media/enemy_12.46c91ff4.png"
+  },
+  {
+    "revision": "69dae7652f511a23ff2ab615f001e9e2",
+    "url": "/fantasia-js/static/media/enemy_13.69dae765.png"
+  },
+  {
+    "revision": "c94bc2c0a578abf82c4abacbe1b27acd",
+    "url": "/fantasia-js/static/media/enemy_14.c94bc2c0.png"
+  },
+  {
+    "revision": "16227af0b4bd907cd14f9cd41db6a0c5",
+    "url": "/fantasia-js/static/media/enemy_15.16227af0.png"
+  },
+  {
+    "revision": "28cad7923ac0ce19689b534e01a486d6",
+    "url": "/fantasia-js/static/media/enemy_16.28cad792.png"
+  },
+  {
+    "revision": "8086a326d67d5e9afca94a855da1986e",
+    "url": "/fantasia-js/static/media/enemy_17.8086a326.png"
+  },
+  {
+    "revision": "ca8a65d89147d4524c4f73280b7d37be",
+    "url": "/fantasia-js/static/media/enemy_18.ca8a65d8.png"
+  },
+  {
+    "revision": "909fb9e494d70ade2573a277621a8fd9",
+    "url": "/fantasia-js/static/media/enemy_19.909fb9e4.png"
+  },
+  {
+    "revision": "fc3dc898a29c1ee648238f48dd5b171f",
+    "url": "/fantasia-js/static/media/enemy_2.fc3dc898.png"
+  },
+  {
+    "revision": "4c90209d285011a30e04c9bb6afc0983",
+    "url": "/fantasia-js/static/media/enemy_20.4c90209d.png"
+  },
+  {
+    "revision": "e7ee4df39b17da79236550335c955c37",
+    "url": "/fantasia-js/static/media/enemy_21.e7ee4df3.png"
+  },
+  {
+    "revision": "34c9c6a9936bfc3f0c45f973c521beb8",
+    "url": "/fantasia-js/static/media/enemy_22.34c9c6a9.png"
+  },
+  {
+    "revision": "554695c1302adce93dfed4068d13d55e",
+    "url": "/fantasia-js/static/media/enemy_23.554695c1.png"
+  },
+  {
+    "revision": "4566accc3113a6fc7f4e35c2141d679c",
+    "url": "/fantasia-js/static/media/enemy_24.4566accc.png"
+  },
+  {
+    "revision": "21a7dc49165f67c83a88277d386a031f",
+    "url": "/fantasia-js/static/media/enemy_25.21a7dc49.png"
+  },
+  {
+    "revision": "28c260af70afe2470a4efa6454eb180d",
+    "url": "/fantasia-js/static/media/enemy_26.28c260af.png"
+  },
+  {
+    "revision": "9422c441c08132b735c77cbc41cb0a03",
+    "url": "/fantasia-js/static/media/enemy_27.9422c441.png"
+  },
+  {
+    "revision": "6b47e745608f29f7fceb082854d9bc65",
+    "url": "/fantasia-js/static/media/enemy_3.6b47e745.png"
+  },
+  {
+    "revision": "54b9a4f47b2aa6f065203edd6d1053a8",
+    "url": "/fantasia-js/static/media/enemy_4.54b9a4f4.png"
+  },
+  {
+    "revision": "e1099da021b5dd301af976e1c5c0dfb9",
+    "url": "/fantasia-js/static/media/enemy_5.e1099da0.png"
+  },
+  {
+    "revision": "c700119391e86c224b8c8c6112ef5773",
+    "url": "/fantasia-js/static/media/enemy_6.c7001193.png"
+  },
+  {
+    "revision": "e846da0c85d6b3ae90250a714171b648",
+    "url": "/fantasia-js/static/media/enemy_7.e846da0c.png"
+  },
+  {
+    "revision": "488d269d8e84299152db46edbf563962",
+    "url": "/fantasia-js/static/media/enemy_8.488d269d.png"
+  },
+  {
+    "revision": "dfcaa044c2e1be1cde734b725424ebdf",
+    "url": "/fantasia-js/static/media/enemy_9.dfcaa044.png"
+  },
+  {
+    "revision": "db9837ebfe684c651c66d9e7b0de5027",
+    "url": "/fantasia-js/static/media/player_1.db9837eb.png"
+  }
+]);
